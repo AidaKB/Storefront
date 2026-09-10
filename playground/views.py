b571cf0b9131c6008f82274e4d87a576c88e1ea5
AidaKB <aida.beheshti81@gmail.com>
2026-09-11 02:38:01 +0330

    Session 2

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
+
+def calculate(x, y):
+    return x + y
 
 
 def say_hello(request):
-    return HttpResponse("Hello World!")
+    res = calculate(10, 20)
+    return render(request, 'hello.html', {'name': 'Aida'})
